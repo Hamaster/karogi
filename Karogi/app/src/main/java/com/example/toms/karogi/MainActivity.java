d6aa914f9dba6eec9a3221d9b3f0923163a6a54b
Toms Balabāns <tomsbalabans@inbox.lv>
2016-05-25 23:00:49 +0300

    work on menu

--- Karogi/app/src/main/java/com/example/toms/karogi/MainActivity.java
+++ Karogi/app/src/main/java/com/example/toms/karogi/MainActivity.java
@@ -1,11 +1,40 @@
 package com.example.toms.karogi;
 
+import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends Activity {
+
+    ImageButton button_5;
+    ImageButton button_10;
+    ImageButton button_15;
+    ImageButton button_20;
+    ImageButton button_50;
+    ImageButton visasValstis;
+    ImageButton afrika;
+    ImageButton australijaUnOkeanija;
+    ImageButton ziemelAmerika;
+    ImageButton azija;
+    ImageButton dienvidAmerika;
+    ImageButton eiropa;
+    ImageButton rekordi;
+    ImageButton sakt;
+
+    boolean btn5, btn10, btn15, btn20, btn50 = false;
+    boolean btnVisasValstis, btnAfrika, btnAustralijaUnOkeanija, btnZiemelAmerika, btnAzija, btnDienvidAmerika, btnEiropa = false;
+    boolean saktIsActive = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,9 +42,318 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+
+        addListenersForMenuButtons();
+        addListenersOnNumberButtons();
+        addListenersForAreaButtons();
 
     }
+    private boolean evaluateSakt(){
+        saktIsActive = (btn5 || btn10 || btn15 || btn20 || btn50) && (btnVisasValstis || btnAfrika || btnAustralijaUnOkeanija || btnZiemelAmerika || btnAzija || btnDienvidAmerika || btnEiropa);
+        return saktIsActive;
+    }
+
+    private void saktBackgroundCheck(){
+        if(evaluateSakt()) sakt.setBackgroundResource(R.drawable.sakt_active);
+        else sakt.setBackgroundResource(R.drawable.sakt_unactive);
+    }
+
+    public void addListenersForMenuButtons(){
+        rekordi = (ImageButton) findViewById(R.id.imageButton_rekordi);
+        sakt = (ImageButton) findViewById(R.id.imageButton_sakt);
+
+        rekordi.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //taketh you to records
+            }
+        });
+
+        sakt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(evaluateSakt()){
+                    //taketh you to quiz
+                }
+                else{
+                    //show toast
+                    Toast.makeText(MainActivity.this, "Izvēlies jautājumu skaitu un valstu reģionu",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    public void addListenersForAreaButtons() {
+        visasValstis = (ImageButton) findViewById(R.id.imageButton_visas_valstis);
+        afrika = (ImageButton) findViewById(R.id.imageButton_afrika);
+        australijaUnOkeanija = (ImageButton) findViewById(R.id.imageButton_australija_un_okeanija);
+        ziemelAmerika = (ImageButton) findViewById(R.id.imageButton_ziemel_amerika);
+        azija = (ImageButton) findViewById(R.id.imageButton_azija);
+        dienvidAmerika = (ImageButton) findViewById(R.id.imageButton_dienvid_amerika);
+        eiropa = (ImageButton) findViewById(R.id.imageButton_eiropa);
+
+        visasValstis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!btnVisasValstis) {
+                    visasValstis.setBackgroundResource(R.drawable.visas_valstis_selected);
+                    afrika.setBackgroundResource(R.drawable.afrika_selected);
+                    australijaUnOkeanija.setBackgroundResource(R.drawable.australija_un_okeanija_selected);
+                    ziemelAmerika.setBackgroundResource(R.drawable.ziemel_amerika_selected);
+                    azija.setBackgroundResource(R.drawable.azija_selected);
+                    dienvidAmerika.setBackgroundResource(R.drawable.dienvid_amerika_selected);
+                    eiropa.setBackgroundResource(R.drawable.eiropa_selected);
+
+                    btnVisasValstis = true;
+                    btnAfrika = true;
+                    btnAustralijaUnOkeanija = true;
+                    btnZiemelAmerika = true;
+                    btnAzija = true;
+                    btnDienvidAmerika = true;
+                    btnEiropa = true;
+
+                    saktBackgroundCheck();
+                }
+                else {
+                    visasValstis.setBackgroundResource(R.drawable.visas_valstis_unselected);
+                    afrika.setBackgroundResource(R.drawable.afrika_unselected);
+                    australijaUnOkeanija.setBackgroundResource(R.drawable.australija_un_okeanija_unselected);
+                    ziemelAmerika.setBackgroundResource(R.drawable.ziemel_amerika_unselected);
+                    azija.setBackgroundResource(R.drawable.azija_unselected);
+                    dienvidAmerika.setBackgroundResource(R.drawable.dienvid_amerika_unselected);
+                    eiropa.setBackgroundResource(R.drawable.eiropa_unselected);
+
+                    btnVisasValstis = false;
+                    btnAfrika = false;
+                    btnAustralijaUnOkeanija = false;
+                    btnZiemelAmerika = false;
+                    btnAzija = false;
+                    btnDienvidAmerika = false;
+                    btnEiropa = false;
+
+                    saktBackgroundCheck();
+                }
+            }
+        });
+        afrika.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btnAfrika == false){
+                    afrika.setBackgroundResource(R.drawable.afrika_selected);
+                    btnAfrika = true;
+                    if((btnAfrika && btnAustralijaUnOkeanija && btnZiemelAmerika && btnAzija && btnDienvidAmerika && btnEiropa) == true){
+                        visasValstis.setBackgroundResource(R.drawable.visas_valstis_selected);
+                        btnVisasValstis = true;
+                    }
+                    saktBackgroundCheck();
+                }
+                else{
+                    afrika.setBackgroundResource(R.drawable.afrika_unselected);
+                    btnAfrika = false;
+                    if(btnVisasValstis) {
+                        visasValstis.setBackgroundResource(R.drawable.visas_valstis_unselected);
+                        btnVisasValstis = false;
+                    }
+                    saktBackgroundCheck();
+                }
+            }
+        });
+
+        australijaUnOkeanija.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btnAustralijaUnOkeanija == false){
+                    australijaUnOkeanija.setBackgroundResource(R.drawable.australija_un_okeanija_selected);
+                    btnAustralijaUnOkeanija = true;
+                    if((btnAfrika && btnAustralijaUnOkeanija && btnZiemelAmerika && btnAzija && btnDienvidAmerika && btnEiropa) == true){
+                        visasValstis.setBackgroundResource(R.drawable.visas_valstis_selected);
+                        btnVisasValstis = true;
+                    }
+                    saktBackgroundCheck();
+                }
+                else{
+                    australijaUnOkeanija.setBackgroundResource(R.drawable.australija_un_okeanija_unselected);
+                    btnAustralijaUnOkeanija = false;
+                    if(btnVisasValstis) {
+                        visasValstis.setBackgroundResource(R.drawable.visas_valstis_unselected);
+                        btnVisasValstis = false;
+                    }
+                    saktBackgroundCheck();
+                }
+            }
+        });
+
+        ziemelAmerika.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btnZiemelAmerika == false){
+                    ziemelAmerika.setBackgroundResource(R.drawable.ziemel_amerika_selected);
+                    btnZiemelAmerika = true;
+                    if((btnAfrika && btnAustralijaUnOkeanija && btnZiemelAmerika && btnAzija && btnDienvidAmerika && btnEiropa) == true){
+                        visasValstis.setBackgroundResource(R.drawable.visas_valstis_selected);
+                        btnVisasValstis = true;
+                    }
+                    saktBackgroundCheck();
+                }
+                else{
+                    ziemelAmerika.setBackgroundResource(R.drawable.ziemel_amerika_unselected);
+                    btnZiemelAmerika = false;
+                    if(btnVisasValstis) {
+                        visasValstis.setBackgroundResource(R.drawable.visas_valstis_unselected);
+                        btnVisasValstis = false;
+                    }
+                    saktBackgroundCheck();
+                }
+            }
+        });
+
+        azija.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btnAzija == false){
+                    azija.setBackgroundResource(R.drawable.azija_selected);
+                    btnAzija = true;
+                    if((btnAfrika && btnAustralijaUnOkeanija && btnZiemelAmerika && btnAzija && btnDienvidAmerika && btnEiropa) == true){
+                        visasValstis.setBackgroundResource(R.drawable.visas_valstis_selected);
+                        btnVisasValstis = true;
+                    }
+                    saktBackgroundCheck();
+                }
+                else{
+                    azija.setBackgroundResource(R.drawable.azija_unselected);
+                    btnAzija = false;
+                    if(btnVisasValstis) {
+                        visasValstis.setBackgroundResource(R.drawable.visas_valstis_unselected);
+                        btnVisasValstis = false;
+                    }
+                    saktBackgroundCheck();
+                }
+            }
+        });
+
+        dienvidAmerika.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btnDienvidAmerika == false){
+                    dienvidAmerika.setBackgroundResource(R.drawable.dienvid_amerika_selected);
+                    btnDienvidAmerika = true;
+                    if((btnAfrika && btnAustralijaUnOkeanija && btnZiemelAmerika && btnAzija && btnDienvidAmerika && btnEiropa) == true){
+                        visasValstis.setBackgroundResource(R.drawable.visas_valstis_selected);
+                        btnVisasValstis = true;
+                    }
+                    saktBackgroundCheck();
+                }
+                else{
+                    dienvidAmerika.setBackgroundResource(R.drawable.dienvid_amerika_unselected);
+                    btnDienvidAmerika = false;
+                    if(btnVisasValstis) {
+                        visasValstis.setBackgroundResource(R.drawable.visas_valstis_unselected);
+                        btnVisasValstis = false;
+                    }
+                    saktBackgroundCheck();
+                }
+            }
+        });
+
+        eiropa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btnEiropa == false){
+                    eiropa.setBackgroundResource(R.drawable.eiropa_selected);
+                    btnEiropa = true;
+                    if((btnAfrika && btnAustralijaUnOkeanija && btnZiemelAmerika && btnAzija && btnDienvidAmerika && btnEiropa) == true){
+                        visasValstis.setBackgroundResource(R.drawable.visas_valstis_selected);
+                        btnVisasValstis = true;
+                    }
+                    saktBackgroundCheck();
+                }
+                else{
+                    eiropa.setBackgroundResource(R.drawable.eiropa_unselected);
+                    btnEiropa = false;
+                    if(btnVisasValstis) {
+                        visasValstis.setBackgroundResource(R.drawable.visas_valstis_unselected);
+                        btnVisasValstis = false;
+                    }
+                    saktBackgroundCheck();
+                }
+            }
+        });
+    }
+
+    public void addListenersOnNumberButtons() {
+        button_5 = (ImageButton) findViewById(R.id.imageButton_5);
+        button_10 = (ImageButton) findViewById(R.id.imageButton_10);
+        button_15 = (ImageButton) findViewById(R.id.imageButton_15);
+        button_20 = (ImageButton) findViewById(R.id.imageButton_20);
+        button_50 = (ImageButton) findViewById(R.id.imageButton_50);
+
+        button_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn5 = true;
+                button_5.setBackgroundResource(R.drawable.i5_selected);
+                button_10.setBackgroundResource(R.drawable.i10_unselected);
+                button_15.setBackgroundResource(R.drawable.i15_unselected);
+                button_20.setBackgroundResource(R.drawable.i20_unselected);
+                button_50.setBackgroundResource(R.drawable.i50_unselected);
+                saktBackgroundCheck();
+            }
+        });
+
+        button_10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn10 = true;
+                button_5.setBackgroundResource(R.drawable.i5_unselected);
+                button_10.setBackgroundResource(R.drawable.i10_selected);
+                button_15.setBackgroundResource(R.drawable.i15_unselected);
+                button_20.setBackgroundResource(R.drawable.i20_unselected);
+                button_50.setBackgroundResource(R.drawable.i50_unselected);
+                saktBackgroundCheck();
+            }
+        });
+
+        button_15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn15 = true;
+                button_5.setBackgroundResource(R.drawable.i5_unselected);
+                button_10.setBackgroundResource(R.drawable.i10_unselected);
+                button_15.setBackgroundResource(R.drawable.i15_selected);
+                button_20.setBackgroundResource(R.drawable.i20_unselected);
+                button_50.setBackgroundResource(R.drawable.i50_unselected);
+                saktBackgroundCheck();
+            }
+        });
+
+        button_20.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn20 = true;
+                button_5.setBackgroundResource(R.drawable.i5_unselected);
+                button_10.setBackgroundResource(R.drawable.i10_unselected);
+                button_15.setBackgroundResource(R.drawable.i15_unselected);
+                button_20.setBackgroundResource(R.drawable.i20_selected);
+                button_50.setBackgroundResource(R.drawable.i50_unselected);
+                saktBackgroundCheck();
+            }
+        });
+
+        button_50.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn50 = true;
+                button_5.setBackgroundResource(R.drawable.i5_unselected);
+                button_10.setBackgroundResource(R.drawable.i10_unselected);
+                button_15.setBackgroundResource(R.drawable.i15_unselected);
+                button_20.setBackgroundResource(R.drawable.i20_unselected);
+                button_50.setBackgroundResource(R.drawable.i50_selected);
+                saktBackgroundCheck();
+            }
+        });
+
+    }
+
 
 }
